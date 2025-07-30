464c6bcada120e9efff3b5462a77ee648a9bf381
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-30 14:13:22 -0300

    feat: fill screen and add fps rate

--- 2Galaxy Fighters/const.py
+++ 2Galaxy Fighters/const.py
@@ -1,1 +1,3 @@
 WIDTH, HEIGHT = 900, 500
+COLORS = {'white': "#ecf0f1", 'black': "#2c3e50", 'red': "#c0392b"}
+FPS = 60
--- 2Galaxy Fighters/main.py
+++ 2Galaxy Fighters/main.py
@@ -2,14 +2,24 @@ import pygame
 from const import *
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Galaxy Fighters")
+
+
+def draw_window():
+    WIN.fill(COLORS["black"])
+    pygame.display.update()
 
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        draw_window()
 
     pygame.quit()
 
