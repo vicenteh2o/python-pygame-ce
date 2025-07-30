50448f6a7b5ca632981dcf3e4189f0a0d194d6b7
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-30 08:53:46 -0300

    feat: add clock to controll refresh rate

--- main.py
+++ main.py
@@ -23,11 +23,21 @@ def main():
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,
                          PLAYER_WIDTH, PLAYER_HEIGHT)
 
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_d]:
+            player.x += PLAYER_VEL
 
         draw(player)
 
