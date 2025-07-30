6bce28515d96452939d9ce68c1480500f0ec38ae
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-30 16:57:40 -0300

    feat: create space ships and add to stage, handle collisions

--- 2Galaxy Fighters/const.py
+++ 2Galaxy Fighters/const.py
@@ -1,3 +1,43 @@
+import pygame
+import os
+
 WIDTH, HEIGHT = 900, 500
 COLORS = {'white': "#ecf0f1", 'black': "#2c3e50", 'red': "#c0392b"}
 FPS = 60
+SPACESHIP_WIDTH = 64
+SPACESHIP_HEIGHT = 32
+VEL = 5
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+BORDER = pygame.Rect(WIDTH/2-5, 0, 10, HEIGHT)
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + yellow.width + VEL < BORDER.x:
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + yellow.height + VEL < HEIGHT:
+        yellow.y += VEL
+
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + red.width + VEL < WIDTH:
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + red.height + VEL < HEIGHT:
+        red.y += VEL
--- 2Galaxy Fighters/main.py
+++ 2Galaxy Fighters/main.py
@@ -1,22 +1,25 @@
 import pygame
-import OS
+
 from const import *
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Galaxy Fighters")
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(
-    OS.path.join('Assets', 'spaceship_yellow.png'))
-RED_SPACESHIP_IMAGE = pygame.image.load(
-    OS.path.join('Assets', 'spaceship_red.png'))
+BORDER = pygame.Rect(WIDTH/2-5, 0, 10, HEIGHT)
 
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill(COLORS["black"])
+    pygame.draw.rect(WIN, COLORS["red"], BORDER)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 
 def main():
+    red = RED_SPACESHIP.get_rect(center=(700, 300))
+    yellow = YELLOW_SPACESHIP.get_rect(center=(100, 300))
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -25,7 +28,10 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
 
     pygame.quit()
 
